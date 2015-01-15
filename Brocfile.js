7151b51dd5c32adf49c57d28a5359d641535a7bd
Kirill Korolyov <kirill.korolyov@gmail.com>
2015-01-15 01:53:19 +0000

    Build: copy bootstrap fonts

--- Brocfile.js
+++ Brocfile.js
@@ -1,14 +1,19 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var pickFiles = require('broccoli-static-compiler')
+var mergeTrees = require('broccoli-merge-trees')
+
 
 var app = new EmberApp();
 
 app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+
 app.import('bower_components/fontawesome/css/font-awesome.min.css');
 
 app.import('bower_components/moment/moment.js');
 app.import('bower_components/ember-data.model-fragments/dist/ember-data.model-fragments.js');
+
 
 
 // Use `app.import` to add additional libraries to the generated
@@ -24,4 +29,20 @@ app.import('bower_components/ember-data.model-fragments/dist/ember-data.model-fr
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-module.exports = app.toTree();
+// Import fontawesome fonts
+var fontawesome = pickFiles('bower_components/fontawesome/fonts', {
+    srcDir: '/',
+    files: [
+        'fontawesome-webfont.ttf',
+        'fontawesome-webfont.woff',
+        'fontawesome-webfont.eot',
+        'FontAwesome.otf',
+        'fontawesome-webfont.svg'
+    ],
+    destDir: '/fonts'
+});
+
+module.exports = mergeTrees([
+    app.toTree(),
+    fontawesome
+]);
