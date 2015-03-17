a182f1bf0e2afd270eb3fe881bf640ce16292012
Kirill Korolyov <kirill.korolyov@gmail.com>
2015-03-17 11:11:40 +0000

    Added album.comment field

--- app/models/album.js
+++ app/models/album.js
@@ -5,6 +5,7 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   artist: DS.attr('string'),
   year: DS.attr('number'),
+  comments: DS.attr('string'),
   first_played: DS.attr('first-played'),
   sources: DS.hasManyFragments('source')
 });
